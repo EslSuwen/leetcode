81d61bd033e5578ffae2fed032587e6cffac18e8
Suwen macOS Yurisa <577014284@qq.com>
2020-06-26 21:48:22 +0800

    翻转每一位数字即可

---
+++ 7.整数反转.java
@@ -0,0 +1,70 @@
+/*
+ * @lc app=leetcode.cn id=7 lang=java
+ *
+ * [7] 整数反转
+ *
+ * https://leetcode-cn.com/problems/reverse-integer/description/
+ *
+ * algorithms
+ * Easy (34.13%)
+ * Likes:    1974
+ * Dislikes: 0
+ * Total Accepted:    385.5K
+ * Total Submissions: 1.1M
+ * Testcase Example:  '123'
+ *
+ * 给出一个 32 位的有符号整数，你需要将这个整数中每位上的数字进行反转。
+ * 
+ * 示例 1:
+ * 
+ * 输入: 123
+ * 输出: 321
+ * 
+ * 
+ * 示例 2:
+ * 
+ * 输入: -123
+ * 输出: -321
+ * 
+ * 
+ * 示例 3:
+ * 
+ * 输入: 120
+ * 输出: 21
+ * 
+ * 
+ * 注意:
+ * 
+ * 假设我们的环境只能存储得下 32 位的有符号整数，则其数值范围为 [−2^31,  2^31 − 1]。请根据这个假设，如果反转后整数溢出那么就返回
+ * 0。
+ * 
+ */
+
+// @lc code=start
+class Solution {
+    public int reverse(int x) {
+        int res = 0;
+        while (x != 0) {
+            int t = x % 10;
+            int newRes = res * 10 + t;
+            //如果数字溢出，直接返回0
+            if ((newRes - t) / 10 != res)
+                return 0;
+            res = newRes;
+            x = x / 10;
+        }
+        return res;
+    }
+
+    /* public int reverse(int x) {
+        long res = 0;
+        while (x != 0) {
+            res = res * 10 + x % 10;
+            x /= 10;
+        }
+        return (int) res == res ? (int) res : 0;
+    }*/
+}
+// https://leetcode-cn.com/problems/reverse-integer/solution/tu-wen-xiang-jie-javadai-ma-de-2chong-shi-xian-fan/
+// @lc code=end
+
